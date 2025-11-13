801878c2b1746555da355515eb42f71d684c5dbd
AliniNofrya <khalifa24ti@mahasiswa.pcr.ac.id>
2025-11-13 07:30:18 +0700

    p9 lagii

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,50 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-<<<<<<< HEAD
 
-=======
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
->>>>>>> d7d3480f9ae784cc4d04e6381a4f004922396251
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-<<<<<<< HEAD
     //
-=======
-
-
-    public function index()
-    {
-        return view('login-form');
-    }
-
-
-    public function login(Request $request)
-    {
-        $request->validate([
-            'email' => 'required',
-            'password' => 'required'
-        ]);
-
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user) {
-            return back()->with('error', 'Email tidak ditemukan.');
-        }
-
-        // jika password salah
-        if (!Hash::check($request->password, $user->password)) {
-            return back()->with('error', 'Password anda salah ');
-        }
-
-        // jika berhasil login
-        // (biasanya set session dulu)
-        session(['user_id' => $user->id]);
-
-        return redirect()->route('dashboard');
-    }
->>>>>>> d7d3480f9ae784cc4d04e6381a4f004922396251
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,12 +12,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-<<<<<<< HEAD
-=======
-    protected $table = 'users';
-    protected $primaryKey = 'id';
-
->>>>>>> d7d3480f9ae784cc4d04e6381a4f004922396251
     /**
      * The attributes that are mass assignable.
      *
@@ -26,11 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-<<<<<<< HEAD
         'password',
-=======
-        'password'
->>>>>>> d7d3480f9ae784cc4d04e6381a4f004922396251
     ];
 
     /**
@@ -40,11 +30,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-<<<<<<< HEAD
         'remember_token',
-=======
-        'remember_token'
->>>>>>> d7d3480f9ae784cc4d04e6381a4f004922396251
     ];
 
     /**
